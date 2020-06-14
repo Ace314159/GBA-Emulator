f28d29f124e0c0995ec7b276166804505c962dde
Akash Munagala <akash.munagala@gmail.com>
2020-06-13 19:46:21 -0700

    Clean up APU register handling

--- core/src/io/apu/mod.rs
+++ core/src/io/apu/mod.rs
@@ -23,35 +23,37 @@ impl APU {
 
 impl APU {
     pub fn read_register(&self, addr: u32) -> u8 {
-        match addr {
-            0x04000080 => self.cnt.read(0),
-            0x04000081 => self.cnt.read(1),
-            0x04000082 => self.cnt.read(2),
-            0x04000083 => self.cnt.read(3),
-            0x04000084 => self.cnt_x.read(0),
-            0x04000085 => self.cnt_x.read(1),
-            0x04000086 => self.cnt_x.read(2),
-            0x04000087 => self.cnt_x.read(3),
-            0x04000088 => self.bias.read(0),
-            0x04000089 => self.bias.read(1),
-            0x0400008A ..= 0x0400008F => 0,
+        assert_eq!(addr >> 12, 0x04000);
+        match addr & 0xFFF {
+            0x080 => self.cnt.read(0),
+            0x081 => self.cnt.read(1),
+            0x082 => self.cnt.read(2),
+            0x083 => self.cnt.read(3),
+            0x084 => self.cnt_x.read(0),
+            0x085 => self.cnt_x.read(1),
+            0x086 => self.cnt_x.read(2),
+            0x087 => self.cnt_x.read(3),
+            0x088 => self.bias.read(0),
+            0x089 => self.bias.read(1),
+            0x08A ..= 0x08F => 0,
             _ => { warn!("Ignoring APU Read at 0x{:08X}", addr); 0 },
         }
     }
 
     pub fn write_register(&mut self, addr: u32, value: u8) {
-        match addr {
-            0x04000080 => self.cnt.write(0, value),
-            0x04000081 => self.cnt.write(1, value),
-            0x04000082 => self.cnt.write(2, value),
-            0x04000083 => self.cnt.write(3, value),
-            0x04000084 => self.cnt_x.write(0, value),
-            0x04000085 => self.cnt_x.write(1, value),
-            0x04000086 => self.cnt_x.write(1, value),
-            0x04000087 => self.cnt_x.write(1, value),
-            0x04000088 => self.bias.write(0, value),
-            0x04000089 => self.bias.write(1, value),
-            0x0400008A ..= 0x0400008F => (),
+        assert_eq!(addr >> 12, 0x04000);
+        match addr & 0xFFF {
+            0x080 => self.cnt.write(0, value),
+            0x081 => self.cnt.write(1, value),
+            0x082 => self.cnt.write(2, value),
+            0x083 => self.cnt.write(3, value),
+            0x084 => self.cnt_x.write(0, value),
+            0x085 => self.cnt_x.write(1, value),
+            0x086 => self.cnt_x.write(1, value),
+            0x087 => self.cnt_x.write(1, value),
+            0x088 => self.bias.write(0, value),
+            0x089 => self.bias.write(1, value),
+            0x08A ..= 0x08F => (),
             _ => warn!("Ignoring APU Write 0x{:08X} = {:02X}", addr, value),
         }
     }
