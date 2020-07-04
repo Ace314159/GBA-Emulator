17dd8ca371ffb53fb024dfdac5af2d44acd10369
Akash Munagala <akash.munagala@gmail.com>
2020-07-04 01:18:14 -0700

    Optimize noise channel generation

--- core/src/io/apu/channel/noise.rs
+++ core/src/io/apu/channel/noise.rs
@@ -6,9 +6,9 @@ pub struct Noise {
     // Registers
     length_reload: u8,
     pub envelope: Envelope,
-    divisor_code: u8,
-    counter_7bit: bool,
-    clock_shift: u8,
+    ratio: u8,
+    counter_width: bool,
+    shift: u8,
     use_length: bool,
     // Sound Generation
     pub length_counter: LengthCounter,
@@ -17,16 +17,14 @@ pub struct Noise {
 }
 
 impl Noise {
-    const DIVISORS: [u16; 8] = [8, 16, 32, 48, 64, 80, 96, 112];
-
     pub fn new() -> Noise {
         Noise {
             // Registers
             length_reload: 0,
             envelope: Envelope::new(),
-            divisor_code: 0,
-            counter_7bit: false,
-            clock_shift: 0,
+            ratio: 0,
+            counter_width: false,
+            shift: 0,
             use_length: false,
             // Sound Generation
             length_counter: LengthCounter::new(),
@@ -36,15 +34,16 @@ impl Noise {
     }
 
     fn calc_reload(&self) -> u16 {
-        std::cmp::max(1, 4 * (Noise::DIVISORS[self.divisor_code as usize] << self.clock_shift))
+        let interval = 64 << self.shift;
+        if self.ratio == 0 { interval / 2 } else { interval * self.ratio as u16 }
     }
 
     pub fn clock(&mut self) {
         if !self.is_on() { return }
         if self.timer.clock_with_reload(self.calc_reload()) {
-            let new_high = (self.lfsr & 0x1) ^ (self.lfsr >> 1 & 0x1);
-            self.lfsr = new_high << 14 | self.lfsr >> 1;
-            if self.counter_7bit { self.lfsr = self.lfsr & !0x40 | new_high << 6 }
+            let carry = self.lfsr & 0x1 != 0;
+            self.lfsr >>= 1;
+            if carry { self.lfsr ^= [0x6000, 0x60][self.counter_width as usize] }
         }
     }
 }
@@ -68,7 +67,7 @@ impl IORegister for Noise {
             0 => 0,
             1 => self.envelope.read(),
             2 | 3 => 0,
-            4 => self.clock_shift << 4 | (self.counter_7bit as u8) << 3 | self.divisor_code,
+            4 => self.shift << 4 | (self.counter_width as u8) << 3 | self.ratio,
             5 => (self.use_length as u8) << 6,
             6 | 7 => 0,
             _ => unreachable!(),
@@ -81,9 +80,9 @@ impl IORegister for Noise {
             1 => self.envelope.write(value),
             2 | 3 => (),
             4 => {
-                self.divisor_code = value & 0x7;
-                self.counter_7bit = value >> 3 & 0x1 != 0;
-                self.clock_shift = value >> 4 & 0xF;
+                self.ratio = value & 0x7;
+                self.counter_width = value >> 3 & 0x1 != 0;
+                self.shift = value >> 4 & 0xF;
             },
             5 => {
                 self.use_length = value >> 6 & 0x1 != 0;
